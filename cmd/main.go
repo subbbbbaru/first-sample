2909379bdcc23a262865dfe13f70335aba238d41
subbbbbaru <subbbbbaru@example.com>
2024-06-27 07:00:57 +1000

    add router

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/gorilla/mux"
 	"github.com/subbbbbaru/first-sample/internal/handlers"
 	"github.com/subbbbbaru/first-sample/internal/repositories"
 	"github.com/subbbbbaru/first-sample/internal/services"
@@ -18,11 +19,13 @@ func main() {
 	service := services.NewItemService(repo)
 	handler := handlers.NewItemHandler(service)
 
-	http.HandleFunc("/items", handler.HandleItems)
+	router := mux.NewRouter()
+	router.HandleFunc("/items", handler.HandleItems)
+	// http.HandleFunc("/items", handler.HandleItems)
 
 	// log.Println("Server is starting on port 8080...")
 	myLog.Info().Println("Server is starting on port 8080...")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(":8080", router); err != nil {
 		myLog.Error().Fatalf("could not start server: %v\n", err)
 	}
 }
